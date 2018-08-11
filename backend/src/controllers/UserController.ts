b5917adc7ef6b63cf351570758a815b04bf7779c
Abbondanzo <peter@abbondanzo.com>
2018-08-11 11:33:36 -0400

    Remove logging

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -6,12 +6,10 @@ import { handleCatchError, handleError } from './helper';
 export default class UserController {
     static async register(req: functions.Request, res: functions.Response) {
         if (!req.body.email) {
-            console.log('invalid');
             handleError('Invalid request body', res, Error('Missing user email'));
             return;
         }
         const user: User = new User(req.body);
-        console.log(user);
         admin
             .auth()
             .createUser(user)

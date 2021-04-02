2e0ec788ac67050b2aacec28b1f84de832812ebe
Abbondanzo <peter@abbondanzo.com>
2021-04-02 00:28:29 -0400

    More setup

---
+++ android/app/src/main/java/com/abbondanzo/journey/persistence/LogEntryEntity.kt
@@ -0,0 +1,16 @@
+package com.abbondanzo.journey.persistence
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.abbondanzo.journey.model.Location
+import org.joda.time.DateTimeZone
+
+@Entity(tableName = "log_entry")
+class LogEntryEntity(
+    @PrimaryKey(autoGenerate = true) val id: Long,
+    @ColumnInfo(name = "title") val title: String,
+    @ColumnInfo(name = "body") val body: String,
+    @ColumnInfo(name = "date") val date: DateTimeZone,
+    @ColumnInfo(name = "location") val location: Location
+)
--- android/app/src/main/java/com/abbondanzo/journey/repository/LogEntryRepository.kt
+++ android/app/src/main/java/com/abbondanzo/journey/repository/LogEntryRepository.kt
@@ -1,9 +1,22 @@
 package com.abbondanzo.journey.repository
 
 import com.abbondanzo.journey.model.LogEntry
+import com.abbondanzo.journey.persistence.LogEntryDao
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
-class LogEntryRepository {
-    fun getLogs(): List<LogEntry> {
-        return listOf()
+class LogEntryRepository(private val logEntryDao: LogEntryDao) {
+    suspend fun getLogs(): Flow<List<LogEntry>> {
+        return logEntryDao.getLogs().map { logs ->
+            logs.map {
+                LogEntry(
+                    id = it.id,
+                    title = it.title,
+                    body = it.body,
+                    date = it.date,
+                    location = it.location
+                )
+            }
+        }
     }
 }

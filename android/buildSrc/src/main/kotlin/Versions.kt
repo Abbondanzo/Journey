9aaa0ae2fc05a2c11e0d2e6b04b844ec19fb6343
Abbondanzo <peter@abbondanzo.com>
2021-04-04 13:29:30 -0400

    Buildsrc

---
+++ android/buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,32 @@
+internal object Versions {
+
+    const val JODA_TIME = "2.10.10"
+
+    object Android {
+        const val ACTIVITY_KTX = "1.2.2"
+        const val APP_COMPAT = "1.2.0"
+        const val CONSTRAINT_LAYOUT = "2.0.2"
+        const val LIFECYCLE = "2.2.0"
+        const val MATERIAL = "1.2.1"
+        const val NAVIGATION = "2.3.4"
+        const val ROOM = "2.2.6"
+
+        object Test {
+            const val ARCH = "2.1.0"
+            const val ESPRESSO = "3.1.0"
+        }
+
+        object Tooling {
+            const val GRADLE = "4.1.3"
+        }
+    }
+
+    object Hilt {
+        const val HILT = "2.33-beta"
+    }
+
+    object Kotlin {
+        const val COROUTINES = "1.3.9"
+        const val STDLIB = "1.4.31"
+    }
+}

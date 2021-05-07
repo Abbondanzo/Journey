71ccf00c3545e726fef2be9c5ee315fd409de633
Abbondanzo <peter@abbondanzo.com>
2021-05-06 17:09:05 -1000

    Update models

--- flutter/lib/models/lat_lng.dart
+++ flutter/lib/models/lat_lng.dart
@@ -6,4 +6,20 @@ class LatLng {
 
   // ignore: non_constant_identifier_names
   static final UNKNOWN = LatLng(-1, -1);
+
+  @override
+  int get hashCode => latitude.hashCode ^ longitude.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is LatLng &&
+          runtimeType == other.runtimeType &&
+          latitude == other.latitude &&
+          longitude == other.longitude;
+
+  @override
+  String toString() {
+    return 'LatLng{latitude: $latitude, longitude: $longitude}';
+  }
 }
--- flutter/lib/models/location.dart
+++ flutter/lib/models/location.dart
@@ -1,3 +1,4 @@
+import 'package:journey/data/location/location_entity.dart';
 import 'package:meta/meta.dart';
 
 import 'lat_lng.dart';
@@ -12,4 +13,30 @@ class Location {
 
   // ignore: non_constant_identifier_names
   static final UNKNOWN = Location('', '', LatLng.UNKNOWN);
+
+  @override
+  int get hashCode => city.hashCode ^ country.hashCode ^ latLng.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Location &&
+          runtimeType == other.runtimeType &&
+          city == other.city &&
+          country == other.country &&
+          latLng == other.latLng;
+
+  @override
+  String toString() {
+    return 'Location{city: $city, country: $country, latLng: $latLng}';
+  }
+
+  LocationEntity toEntity() {
+    return LocationEntity(city, country, latLng.latitude, latLng.longitude);
+  }
+
+  static Location fromEntity(LocationEntity entity) {
+    return Location(
+        entity.city, entity.country, LatLng(entity.latitude, entity.longitude));
+  }
 }

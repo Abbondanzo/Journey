1e141e5193e1310c0781e66d4ed4b643d8c1c4b6
Abbondanzo <peter@abbondanzo.com>
2021-05-11 00:28:20 -0400

    Starter code

--- flutter/lib/widgets/entries/entries_list.dart
+++ flutter/lib/widgets/entries/entries_list.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:journey/blocs/entries/entries.dart';
-import 'package:journey/widgets/entries/entry_item.dart';
+import 'package:journey/models/entry.dart';
+
+import 'entry_item.dart';
+import 'entry_item_loading.dart';
 
 class EntriesList extends StatelessWidget {
   EntriesList({
@@ -10,29 +13,38 @@ class EntriesList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<EntriesBloc, EntriesState>(
-        builder: (BuildContext context, EntriesState state) {
+    return BlocBuilder<EntriesBloc, EntriesState>(builder: (_, state) {
       if (state is EntriesLoading) {
         return this._buildLoading();
       } else if (state is EntriesLoaded) {
-        final entries = state.entries;
-        return ListView.builder(
-          itemCount: entries.length,
-          itemBuilder: (BuildContext context, int index) {
-            final entry = entries[index];
-            return EntryItem(
-                entry: entry,
-                onTap: () {
-                  print("Item $index");
-                });
-          },
-        );
+        return this._buildLoaded(state.entries);
       } else {
-        return Container();
+        return this._buildZeroState();
       }
     });
   }
 
-  // TODO
-  Widget _buildLoading() {}
+  Widget _buildLoading() {
+    return ListView(
+      children: [EntryItemLoading(), EntryItemLoading(), EntryItemLoading()],
+    );
+  }
+
+  Widget _buildLoaded(List<Entry> entries) {
+    return ListView.builder(
+      itemCount: entries.length,
+      itemBuilder: (BuildContext context, int index) {
+        final entry = entries[index];
+        return EntryItem(
+            entry: entry,
+            onTap: () {
+              print("Item $index");
+            });
+      },
+    );
+  }
+
+  Widget _buildZeroState() {
+    return Container();
+  }
 }
--- flutter/lib/widgets/entries/entry_item.dart
+++ flutter/lib/widgets/entries/entry_item.dart
@@ -10,11 +10,19 @@ class EntryItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      onTap: onTap,
-      title: Text(entry.title, key: Key('${entry.id}__title')),
-      subtitle: Text(entry.location.formattedLocation(),
-          key: Key('${entry.id}__subtitle')),
-    );
+    return InkWell(
+        onTap: onTap,
+        child: Column(children: [
+          Text(entry.title, key: Key('${entry.id}__title')),
+          Text(entry.location.formattedLocation(),
+              key: Key('${entry.id}__subtitle'))
+        ]));
+    // return Scaffold(
+    //     body: ListTile(
+    //   onTap: onTap,
+    //   title: Text(entry.title, key: Key('${entry.id}__title')),
+    //   subtitle: Text(entry.location.formattedLocation(),
+    //       key: Key('${entry.id}__subtitle')),
+    // ));
   }
 }
